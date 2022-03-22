3b288c1fb1b036a700cd9efc18d96708fcfbd4dd
hassanezz97 <75791415+hassanezz97@users.noreply.github.com>
2022-03-22 15:57:49 +0200

    Update main_trans.php

--- resources/lang/ar/main_trans.php
+++ resources/lang/ar/main_trans.php
@@ -24,6 +24,6 @@ return [
     'Settings'=>'الاعدادات',
     'Users'=>'المستخدمين',
     'Copyright' => 'جميع الحقوق محفوظة ',
-    'Name_Programer' => 'سمير جمال مورا سوفت'
+    'Name_Programer' => 'حسن عزالدين'
 
 ];
